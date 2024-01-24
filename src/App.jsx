6f46776e3f5dcef6a8b0fd5082da468d3dd739c8
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-01-24 11:24:57 +0100

    added header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h2>Hello</h2>
+      <Header></Header>
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar></Sidebar>
+        </div>
+      </main>
     </>
   );
 }
