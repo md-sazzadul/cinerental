f1705a587a8b0997fa0fde7cc7e54b139b562b66
md-sazzadul <md.sazzadul.islam15@gmail.com>
2025-02-27 19:21:39 +0100

    persist darkMode using localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Page from "./components/Page/Page";
 import { MovieProvider, ThemeContext } from "./context";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("darkMode") === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", darkMode);
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
       <MovieProvider>
-        <Page></Page>
-        <ToastContainer></ToastContainer>
+        <Page />
+        <ToastContainer />
       </MovieProvider>
     </ThemeContext.Provider>
   );
