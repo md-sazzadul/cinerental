827bab70fe93ef6aabbb6c9c8d9935431297ff3f
md-sazzadul <md.sazzadul.islam15@gmail.com>
2025-03-01 23:10:51 +0100

    convert page.jsx to typescript

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -9,12 +9,12 @@ import Header from "../Header/Header";
 import SearchBar from "../SearchBar/SearchBar";
 import Sidebar from "../Sidebar/Sidebar";
 
-const Page = () => {
+const Page: React.FC = () => {
   const { darkMode } = useContext(ThemeContext);
 
   return (
     <div className={`h-full ${darkMode ? "dark" : ""}`}>
-      <Header></Header>
+      <Header />
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
